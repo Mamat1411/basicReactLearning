73259663a027f4ecc06128c52bea07977054dc2a
Mamat1411 <muhammadmujahid14@gmail.com>
2025-10-08 10:22:57 +0700

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Header from './Header'
+
+function Welcome() {
+  return (
+    <h1>Hello, this is welcome component</h1>
+  )
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
-      <p>Learning JSX is fun</p>
+      <Header />
+      <p>This is React's Main Page</p>
     </div>
   )
 }
