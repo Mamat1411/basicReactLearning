5aea172d4e450b5a062fe3b39230513e9e3ee650
Mamat1411 <muhammadmujahid14@gmail.com>
2025-10-13 13:17:14 +0700

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,29 @@ function App() {
   //   setTodos([...todos, "Learn State"])
   // }
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
 
-  const handleClick = () => {
-    alert("Button Clicked");
-  };
+  // const handleClick = () => {
+  //   alert("Button Clicked");
+  // };
 
-  const handleChange = () => {
-    setName(event.target.value);
-  };
+  // const handleChange = () => {
+  //   setName(event.target.value);
+  // };
 
-  const handleSubmit = () => {
-    event.preventDefault();
-    alert(`Email sent: ${email}`);
-  };
+  // const handleSubmit = () => {
+  //   event.preventDefault();
+  //   alert(`Email sent: ${email}`);
+  // };
 
-  const sayHello = (name) => {
-    alert(`Hello, ${name}!`);
-  };
+  // const sayHello = (name) => {
+  //   alert(`Hello, ${name}!`);
+  // };
+
+  // const isLoggedIn = true;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const showMessage = true;
 
   return (
     <div>
@@ -60,7 +64,7 @@ function App() {
       </ul>
       <button onClick={addTodo}>Add To Do</button> */}
 
-      <button onClick={handleClick}>Click Me!</button>
+      {/* <button onClick={handleClick}>Click Me!</button>
 
       <br />
       <br />
@@ -74,9 +78,33 @@ function App() {
       </form>
 
       <button onClick={() => sayHello("Mamat")}>Greet Mamat</button>
-      <button onClick={() => sayHello("Muhammad")}>Greet Muhammad</button>
+      <button onClick={() => sayHello("Muhammad")}>Greet Muhammad</button> */}
+
+      { isLoggedIn ? <h1>Dashboard</h1> : <h1>Login Page</h1>}
+
+      <h1>Hello, React!</h1>
+      { showMessage && <p>This message occurs only when showMessage is true</p> }
+
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        { isLoggedIn ? "Logout" : "Login" }
+      </button>
     </div>
-  )
+  );
+
+  // if (isLoggedIn) {
+  //   return (
+  //     <div>
+  //       <h1>Welcome Back</h1>
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div>
+  //       <h1>Log In First</h1>
+  //     </div>
+  //   );
+  // }
+
 }
 
 export default App
