4dec044fc591ec7d5cc9f8f976cd631dddc6bd1b
Mamat1411 <muhammadmujahid14@gmail.com>
2025-10-13 13:06:35 +0700

    Event Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// import { useState } from 'react'
 import { useState } from 'react'
 import './App.css'
 import Header from './Header'
@@ -5,13 +6,33 @@ import UserCard from './UserCard'
 import Welcome from './Welcome'
 
 function App() {
-  const [message, setMessage] = useState("Hello, React");
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState(["Learn React", "Learn JSX"]);
+  // const [message, setMessage] = useState("Hello, React");
+  // const [count, setCount] = useState(0);
+  // const [todos, setTodos] = useState(["Learn React", "Learn JSX"]);
 
-  const addTodo = () => {
-    setTodos([...todos, "Learn State"])
-  }
+  // const addTodo = () => {
+  //   setTodos([...todos, "Learn State"])
+  // }
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleClick = () => {
+    alert("Button Clicked");
+  };
+
+  const handleChange = () => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    event.preventDefault();
+    alert(`Email sent: ${email}`);
+  };
+
+  const sayHello = (name) => {
+    alert(`Hello, ${name}!`);
+  };
 
   return (
     <div>
@@ -24,7 +45,7 @@ function App() {
       {/* <UserCard name="Muhammad" age={26} />
       <UserCard name="Mamat" age={26} />
       <UserCard name="Mujahid" age={26} /> */}
-      <h1>{message}</h1>
+      {/* <h1>{message}</h1>
       <button onClick={() => setMessage("State has changed!")}>Change message</button>
 
       <h1>Count: {count}</h1>
@@ -37,7 +58,23 @@ function App() {
           <li key={index}>{todo}</li>
         ))}
       </ul>
-      <button onClick={addTodo}>Add To Do</button>
+      <button onClick={addTodo}>Add To Do</button> */}
+
+      <button onClick={handleClick}>Click Me!</button>
+
+      <br />
+      <br />
+
+      <input type="text" placeholder='Insert name...' onChange={handleChange} />
+      <p>Hello, {name}</p>
+
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder='Insert email...' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <button type="submit">Send</button>
+      </form>
+
+      <button onClick={() => sayHello("Mamat")}>Greet Mamat</button>
+      <button onClick={() => sayHello("Muhammad")}>Greet Muhammad</button>
     </div>
   )
 }
