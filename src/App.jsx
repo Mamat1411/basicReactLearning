c93ff282f81737e198024a164639468274fc6499
Mamat1411 <muhammadmujahid14@gmail.com>
2025-10-13 09:13:28 +0700

    state and UseState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './Header'
 import UserCard from './UserCard'
 import Welcome from './Welcome'
 
 function App() {
+  const [message, setMessage] = useState("Hello, React");
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["Learn React", "Learn JSX"]);
+
+  const addTodo = () => {
+    setTodos([...todos, "Learn State"])
+  }
+
   return (
     <div>
       {/* <Header />
@@ -12,9 +21,23 @@ function App() {
       {/* <Welcome name="Muhammad" />
       <Welcome name="Mamat" /> */}
 
-      <UserCard name="Muhammad" age={26} />
+      {/* <UserCard name="Muhammad" age={26} />
       <UserCard name="Mamat" age={26} />
-      <UserCard name="Mujahid" age={26} />
+      <UserCard name="Mujahid" age={26} /> */}
+      <h1>{message}</h1>
+      <button onClick={() => setMessage("State has changed!")}>Change message</button>
+
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <button onClick={() => setCount(count - 1)}>Substract</button>
+
+      <h1>To Do List</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <button onClick={addTodo}>Add To Do</button>
     </div>
   )
 }
